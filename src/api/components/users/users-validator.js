624b42a86a97b713a7b917f84e9a9f3d843606db
Donnie Santo <donnie.535230153@stu.ac.id>
2024-04-26 15:15:55 +0700

    first commit

---
+++ src/api/components/users/users-validator.js
@@ -0,0 +1,25 @@
+const joi = require('joi');
+
+module.exports = {
+  createUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+      password: joi.string().min(6).max(32).required().label('Password'),
+      passwordConfirmation: joi
+        .string()
+        .valid(joi.ref('password'))
+        .required()
+        .label('password confirm')
+        .messages({'any.only': 'Passwords not match',
+        }),
+    },
+  },
+
+  updateUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+    },
+  },
+};
